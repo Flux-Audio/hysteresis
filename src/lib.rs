1d7719c690b5b48115ced8ad78dbc8aa7fa495fc
Lorenzo Panieri <panierilorenzo@gmail.com>
2020-12-27 02:06:30 +0100

    prototyping hysteresis

--- src/lib.rs
+++ src/lib.rs
@@ -15,11 +15,6 @@ use std::sync::Arc;
 
 mod compute; // contains processing functions
 
-enum BiasMode{  TAPE,    TUBE }
-enum CrossMode{ DIGITAL, ANALOG }
-enum HystMode{  DIGITAL, TAPE1, TAPE2, TUBE }
-enum SatMode{   TAPE1,   TAPE2, CLIP,  TUBE }
-
 // Plugin struct, this is where the processing happens
 struct Effect {
     // Store a handle to the plugin's parameter object.
@@ -31,18 +26,10 @@ struct Effect {
     rate: f32,
 
     // differential variables
-    xl_p: f32,
-    xr_p: f32,
-    w_p_l_p: f32,
-    w_m_l_p: f32,
-    w_p_r_p: f32,
-    w_m_r_p: f32,
-    hyst_l_p: f32,
-    hyst_r_p: f32,
-    xl_q_p: f32,
-    xr_q_p: f32,
-    yl_p: f32,
-    yr_p: f32,
+    xl_q_z1: f32,
+    xr_q_z1: f32,
+    xl_h_z1: f32,
+    xr_h_z1: f32,
 }
 
 // Plugin parameters, this is where the UI happens
@@ -77,18 +64,10 @@ impl Default for Effect {
             sr: 44100.0,
             rate: 1.0/44100.0,
 
-            xl_p: 0.0,
-            xr_p: 0.0,
-            w_p_l_p: 0.0,
-            w_m_l_p: 0.0,
-            w_p_r_p: 0.0,
-            w_m_r_p: 0.0,
-            hyst_l_p: 0.0,
-            hyst_r_p: 0.0,
-            xl_q_p: 0.0,
-            xr_q_p: 0.0,
-            yl_p: 0.0,
-            yr_p: 0.0,
+            xl_q_z1: 0.0,
+            xr_q_z1: 0.0,
+            xl_h_z1: 0.0,
+            xr_h_z1: 0.0,
         }
     }
 }
@@ -153,175 +132,30 @@ impl Plugin for Effect {
 
         // process
         for ((left_in, right_in), (left_out, right_out)) in stereo_in.zip(stereo_out) {
-
-            // === get all params ==============================================
-            let pre_gain = self.params.pre_gain.get().powf(3.0)*8.0;
-            let drive = self.params.drive.get()*4.9 + 0.1;
-            let bias = self.params.bias.get()*2.0 - 1.0;
-            let bias_mode = if self.params.bias_mode.get() < 0.5 {
-                BiasMode::TAPE
-            } else {
-                BiasMode::TUBE
-            };
-            let cross_amt = self.params.cross_amt.get();
-            let cross_width = self.params.cross_width.get();
-            let cross_mode = if self.params.cross_mode.get() < 0.5 {
-                CrossMode::DIGITAL
-            } else {
-                CrossMode::ANALOG
-            };
-            let hyst_amt = self.params.hyst_amt.get().powf(3.0);
-            let hyst_param = self.params.hyst_param.get();
-            let hyst_mode = match self.params.hyst_mode.get() {
-                0.0..=0.25 => HystMode::DIGITAL,
-                0.25..=0.5 => HystMode::TAPE1,
-                0.5..=0.75 => HystMode::TAPE2,
-                _ => HystMode::TUBE
-            };
             let quantum = self.params.quantum.get();
-            let sat_mode = match self.params.sat_mode.get() {
-                0.0..=0.25 => SatMode::TAPE1,
-                0.25..=0.5 => SatMode::TAPE2,
-                0.5..=0.75 => SatMode::CLIP,
-                _ => SatMode::TUBE
-            };
-            let dry_wet = self.params.dry_wet.get();
-            let cut = self.params.cut.get().sqrt().sqrt();
-            let post_gain = self.params.post_gain.get()*2.0;
-
-            // === pre-gain and drive ==========================================
-            let dry_l = *left_in*pre_gain;
-            let dry_r = *right_in*pre_gain;
-            let mut xl = dry_l*drive;
-            let mut xr = dry_r*drive;
-
-            // === crossover ===================================================
-            match cross_mode {
-                CrossMode::DIGITAL => {
-                    xl = compute::digital_xover(xl, cross_amt, cross_width);
-                    xr = compute::digital_xover(xr, cross_amt, cross_width);
-                },
-                CrossMode::ANALOG => {
-                    xl = compute::analog_xover(xl, cross_amt, cross_width);
-                    xr = compute::analog_xover(xr, cross_amt, cross_width);
-                }
-            }
+            //let xo_amt = self.params.cross_amt.get().sqrt().sqrt().sqrt().sqrt().sqrt().sqrt().sqrt().sqrt();
+            let xo_w = self.params.cross_width.get();
 
             // === hysteresis ==================================================
-            // HACK: legacy functionality implemented by returning tape 2 in the
-            // first half of the tuple
-            let (w_p_l, w_m_l) = match hyst_mode {
-                HystMode::DIGITAL => compute::digital_window(xl, hyst_amt, hyst_param),
-                HystMode::TAPE1 => compute::tape_window_1(xl, hyst_amt, hyst_param),
-                HystMode::TAPE2 => (compute::tape_window_2(xl, hyst_amt, hyst_param, 
-                    compute::diff(xl, self.xl_p, self.rate)), 0.0),
-                HystMode::TUBE => compute::tube_window(xl, hyst_amt, hyst_param),
-                _ => (0.0, 0.0)
-            };
-            let (w_p_r, w_m_r) = match hyst_mode {
-                HystMode::DIGITAL => compute::digital_window(xr, hyst_amt, hyst_param),
-                HystMode::TAPE1 => compute::tape_window_1(xr, hyst_amt, hyst_param),
-                HystMode::TAPE2 => (compute::tape_window_2(xr, hyst_amt, hyst_param, 
-                    compute::diff(xr, self.xr_p, self.rate)), 0.0),
-                HystMode::TUBE => compute::tube_window(xr, hyst_amt, hyst_param),
-                _ => (0.0, 0.0)
-            };
-
-            // treat legacy mode separately
-            match hyst_mode{
-                HystMode::TAPE2 => {
-                    xl = w_p_l;
-                    xr = w_p_r;
-                },
-                _ => {
-                    // differentiate window functions
-                    let d_w_p_l = w_p_l - self.w_p_l_p;
-                    let d_w_m_l = w_m_l - self.w_m_l_p;
-                    let d_w_p_r = w_p_r - self.w_p_r_p;
-                    let d_w_m_r = w_m_r - self.w_m_r_p;
-                    self.w_p_l_p = w_p_l;
-                    self.w_m_l_p = w_m_l;
-                    self.w_p_r_p = w_p_r;
-                    self.w_m_r_p = w_m_r;
-
-                    // differentiate input
-                    let d_xl = xl - self.xl_p;
-                    let d_xr = xr - self.xr_p;
-                    self.xl_p = xl;
-                    self.xr_p = xr;
-
-                    // transfer window delta to x delta
-                    let hyst_l: f32;
-                    let hyst_r: f32;
-                    if d_xl > 0.0{
-                        hyst_l = self.hyst_l_p + d_w_p_l;
-                    } else {
-                        hyst_l = self.hyst_l_p + d_w_m_l;
-                    }
-                    if d_xr > 0.0{
-                        hyst_r = self.hyst_l_p + d_w_p_r;
-                    } else {
-                        hyst_r = self.hyst_r_p + d_w_m_r;
-                    }
-                    self.hyst_l_p = hyst_l;
-                    self.hyst_r_p = hyst_r;
-
-                    xl = hyst_l;
-                    xr = hyst_r;
-                }
-            }
+            let mut xl = *left_in;
+            let mut xr = *right_in;
+            
+            xl = self.xl_h_z1 + compute::analog_xover(xl - self.xl_h_z1, 0.9997, xo_w);
+            xr = self.xr_h_z1 + compute::analog_xover(xr - self.xr_h_z1, 0.9997, xo_w);
+            self.xl_h_z1 = xl;
+            self.xr_h_z1 = xr;
 
             // === stochastic quantization =====================================
-            xl = compute::x_quant(xl, self.xl_p, self.rate, quantum, &mut self.rng);
-            xr = compute::x_quant(xr, self.xr_p, self.rate, quantum, &mut self.rng);
+            xl = compute::x_quant(xl, self.xl_q_z1, self.rate, quantum, &mut self.rng);
+            xr = compute::x_quant(xr, self.xr_q_z1, self.rate, quantum, &mut self.rng);
+            self.xl_q_z1 = xl;
+            self.xr_q_z1 = xr;
 
             // === saturation ==================================================
-            let mut anti_drive: f32 = 0.0;  // saturate the drive input to scale 
-                                            // the output back to unity gain
-            match sat_mode{
-                SatMode::TAPE1 => { 
-                    xl = compute::tape_sat_1(xl); 
-                    xr = compute::tape_sat_1(xr);
-                    anti_drive = 1.0/compute::tape_sat_1(drive);
-                },
-                SatMode::TAPE2 => {
-                    xl = compute::tape_sat_2(xl); 
-                    xr = compute::tape_sat_2(xr);
-                    anti_drive = 1.0/compute::tape_sat_2(drive);
-                },
-                SatMode::TUBE => {
-                    xl = compute::tube_sat(xl); 
-                    xr = compute::tube_sat(xr);
-                    anti_drive = 1.0/compute::tube_sat(drive);
-                },
-                SatMode::CLIP => {
-                    xl = compute::soft_clip(xl); 
-                    xr = compute::soft_clip(xr);
-                    anti_drive = 1.0/compute::soft_clip(drive);
-                },
-                _ => ()
-            }
+            *left_out = compute::mag_sat_1(xl);
+            *right_out = compute::mag_sat_1(xr);
 
-            // === anti-drive ==================================================
-            xl *= anti_drive;
-            xr *= anti_drive;
-
-            // === dry / wet ===================================================
-            xl = xl*dry_wet + dry_l*(1.0 - dry_wet);
-            xr = xr*dry_wet + dry_r*(1.0 - dry_wet);
-
-            // === post-eq =====================================================
-            xl = compute::play(xl, self.yl_p, cut);
-            xr = compute::play(xr, self.yr_p, cut);
-            self.yl_p = xl;
-            self.yr_p = xr;
-
-            // === post gain ===================================================
-            let yl = xl*post_gain;
-            let yr = xr*post_gain;
-
-            *left_out = yl;
-            *right_out = yr;
+            // === self-erasure ================================================
         }
     }
 
